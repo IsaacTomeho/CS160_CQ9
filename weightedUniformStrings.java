1520f37a6223158cfe5deefd226636fcded4aa0b
Isaac Tomeho <119877312+IsaacTomeho@users.noreply.github.com>
2023-12-08 12:22:55 -0500

    Create weightedUniformStrings.java

---
+++ weightedUniformStrings.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'weightedUniformStrings' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER_ARRAY queries
+     */
+
+    public static List<String> weightedUniformStrings(String s, List<Integer> queries) {
+    // Write your code here
+        Set<Integer> weights = new HashSet<>();
+        List<String> result = new ArrayList<>();
+
+        // Previous character and its weight
+        char previous = ' ';
+        int currentWeight = 0;
+
+        // calculate weights
+        for (char ch : s.toCharArray()) {
+            // if  current char is same as previous, increase weight
+            if (ch == previous) {
+                currentWeight += ch - 'a' + 1;
+            } else {
+                // start a new uniform substring
+                currentWeight = ch - 'a' + 1;
+                previous = ch;
+            }
+            weights.add(currentWeight);
+        }
+
+        // query check
+        for (int query : queries) {
+            result.add(weights.contains(query) ? "Yes" : "No");
+        }
+
+        return result;
+    }
+    
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<String> result = Result.weightedUniformStrings(s, queries);
+
+        bufferedWriter.write(
+            result.stream()
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
